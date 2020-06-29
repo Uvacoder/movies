3705ac8ae21bdb85d7e784627f602f2a8047a93a
patrykbura <patrykbura@gmail.com>
2020-06-29 19:56:45 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router} from "react-router-dom";
+import {Switch,Route} from "react-router-dom";
 import WelcomePage from './WelcomePage/WelcomePage'
 
 function App() {
   return (
-    <div className="App">
-      <WelcomePage/>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <WelcomePage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Registration/Registration.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Registration.css"
+
+class Registration extends React.Component {
+    constructor (props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className='registerContainer'>Here goes register component</div>
+        )
+    }
+}
+
+export default Registration;
