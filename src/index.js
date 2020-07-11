3086aa7389ba1804666756ee9ab1209f38eee04f
Your NamePatryk <patrykbura@gmail.com>
2020-07-11 19:54:58 +0200

    setting up REDUX

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk';
+import allReducers from './Reducers/allReducers';
+
+const store = createStore(
+  allReducers,
+  compose(
+    applyMiddleware(thunk), 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ store }>
+      <App /> 
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
