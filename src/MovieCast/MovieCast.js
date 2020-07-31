ce658b776e0c7ac6937115117857ef524fd6d307
patrykbura <patrykbura@gmail.com>
2020-07-31 19:34:53 +0200

    addes cast component

---
+++ src/MovieCast/MovieCast.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import "./MovieCast.scss"
+import Carousel from 'react-multi-carousel';
+
+
+const MovieCast = (props) => {
+
+    const {
+        castItems
+    } = props;
+
+    const responsive = {
+        all: {
+          breakpoint: { max: 4000, min: 0 },
+          items: 5
+        }
+      };  
+
+    const renderCastBlock = (item) => {
+    return (
+        <div className='movie-cast__container'>
+        <img 
+            className='movie-cast__container-image' 
+            src={ `https://image.tmdb.org/t/p/w500${ item?.profile_path }`} 
+            alt=''
+        />
+        <div className='movie-cast__container-name'>{ item?.name}</div>
+        <div className='movie-cast__container-character'>{ item?.character}</div>
+        </div>
+        )
+    }
+
+    return (
+        <div className='movie-cast'>
+            <div style={{width: '100%'}}>
+        <Carousel 
+          responsive={responsive}
+          infinite={false}
+          autoPlay={false}
+        >
+          { castItems.filter((item) => item.profile_path).map((item) => renderCastBlock(item)) }
+        </Carousel>
+      </div>   
+        </div>
+    );
+};
+
+MovieCast.defaultProps = {
+    castItems: []
+}
+
+export default MovieCast
+
--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -6,28 +6,38 @@ import { fetchMovieDetails } from '../Actions/MovieActions'
 import { Divider } from 'antd'
 import MovieHeader from '../MovieHeader/MovieHeader'
 import MovieOverwiev from '../MovieOverwiev/MovieOverwiev'
+import MovieCast from '../MovieCast/MovieCast'
 
-const TEMP_MOVIE_ID = 979;
+const TEMP_MOVIE_ID = 23168;
 const BACKDROP_API_PATH = 'https://image.tmdb.org/t/p/original'
 const POSTER_PATH = 'https://image.tmdb.org/t/p/w500'
 
 class MovieDetails extends React.Component {
   constructor(props) {
     super(props);
+
   }
 
   componentDidMount() {
     this.props.fetchMovieDetails(TEMP_MOVIE_ID);
   }
 
+  filterDirector = () => {
+    return this.props.details.credits?.crew?.filter((item) => item.job === "Director")?.[0]?.name
+  }
+  filterWriters = () => {
+    return this.props.details.credits?.crew?.filter((item) => item.department === "Writing")?.map((item) => item.name).join(', ')
+  }
+
   render() {
-    console.log(this.props.details)
+    
+    
     return (
         <div className='movie-details-container'>
             <div className='movie-details-container__header'> 
                 <MovieHeader 
                     backDropPath={`${BACKDROP_API_PATH}${this.props.details.backdrop_path}`} 
-                    title={this.props.details.original_title} 
+                    title={this.props.details.title || this.props.details.original_title} 
                     tagline={this.props.details.tagline ? this.props.details.tagline : this.props.details.original_title}
                     voteAverage={this.props.details.vote_average}
                     popularity={this.props.details.popularity}
@@ -41,8 +51,8 @@ class MovieDetails extends React.Component {
                     genres={this.props.details.genres?.map((item) => item.name).join(', ')}
                     runtime={this.props.details.runtime}
                     country={this.props.details.production_countries?.map((item) => item.name).join(', ')}
-                    director={this.props.details.credits?.crew?.filter((item) => item.job === 'director').name}
-                    writers={'1'}
+                    director={this.filterDirector()}
+                    writers={this.filterWriters()}
                     budget={`${this.props.details.budget} $`}
                     revenue={`${this.props.details.revenue} $`}
                     languages={this.props.details.spoken_languages?.map((item) => item.name).join(', ')}
@@ -50,13 +60,8 @@ class MovieDetails extends React.Component {
                 />
             </div>
             <Divider className='movie-details-container__divider' orientation='left'>CAST</Divider>
-            <div className='test2'> 
-                <div>CAST:</div>
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
-                <div>4</div>
-                <div>5</div>
+            <div className='movie-details-container__cast'> 
+                <MovieCast castItems={this.props.details.credits?.cast}/>
             </div>
             <Divider className='movie-details-container__divider' orientation='left'>WATCH TRAILER</Divider>
             <div>TRAILER</div>
