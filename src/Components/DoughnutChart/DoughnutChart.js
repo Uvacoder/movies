c2369b3e0dc59196a2adbd4c4d3c08e4af99264a
Your NamePatryk <patrykbura@gmail.com>
2020-10-12 18:52:43 +0200

    pre PR fixes

--- src/Components/DoughnutChart/DoughnutChart.js
+++ src/Components/DoughnutChart/DoughnutChart.js
@@ -26,55 +26,55 @@ const DoughnutChart = (props) => {
         background
     } = props;
     
-    const getColor = () => {
-      if (data <= (0.1 * maxValue)) {
-        return COLOR_OF_10_PERCENT
-      }
-      if (data <= (0.2 * maxValue)) {
-        return COLOR_OF_20_PERCENT
-      }
-      if (data <= (0.3 * maxValue)) {
-        return COLOR_OF_30_PERCENT
-      }
-      if (data <= (0.4 * maxValue)) {
-        return COLOR_OF_40_PERCENT
-      }
-      if (data <= (0.5 * maxValue)) {
-        return COLOR_OF_50_PERCENT
-      }
-      if (data <= (0.6 * maxValue)) {
-        return COLOR_OF_60_PERCENT
-      }
-      if (data <= (0.7 * maxValue)) {
-        return COLOR_OF_70_PERCENT
-      }
-      if (data <= (0.8 * maxValue)) {
-        return COLOR_OF_80_PERCENT
-      }
-      if (data <= (0.9 * maxValue)) {
-        return COLOR_OF_90_PERCENT
-      }
-      if (data >=  maxValue) {
-        return COLOR_OF_100_PERCENT
-      }
+  const getColor = () => {
+    if (data <= (0.1 * maxValue)) {
+      return COLOR_OF_10_PERCENT
     }
+    if (data <= (0.2 * maxValue)) {
+      return COLOR_OF_20_PERCENT
+    }
+    if (data <= (0.3 * maxValue)) {
+      return COLOR_OF_30_PERCENT
+    }
+    if (data <= (0.4 * maxValue)) {
+      return COLOR_OF_40_PERCENT
+    }
+    if (data <= (0.5 * maxValue)) {
+      return COLOR_OF_50_PERCENT
+    }
+    if (data <= (0.6 * maxValue)) {
+      return COLOR_OF_60_PERCENT
+    }
+    if (data <= (0.7 * maxValue)) {
+      return COLOR_OF_70_PERCENT
+    }
+    if (data <= (0.8 * maxValue)) {
+      return COLOR_OF_80_PERCENT
+    }
+    if (data <= (0.9 * maxValue)) {
+      return COLOR_OF_90_PERCENT
+    }
+    if (data >=  maxValue) {
+      return COLOR_OF_100_PERCENT
+    }
+  }
 
-    return (
-      <PieChart
-      data={[{ value: Math.min(data, maxValue), color: getColor()}]}
-      totalValue={maxValue}
-      lineWidth={DOUGHNUT_CHART_LINE_WIDTH}
-      label={({ dataEntry }) => percent ? `${dataEntry.value}%` : dataEntry.value}
-      labelStyle={{
-        fontSize,
-        fill: fontColor,
-      }}
-      labelPosition={DOUGHNUT_CHART_LABEL_POSITION}
-      background={background}
-      startAngle={DOUGHNUT_CHART_START_ANGLE}
-      onClick={ props.onClick }
-      />     
-    );
+  return (
+    <PieChart
+    data={[{ value: Math.min(data, maxValue), color: getColor()}]}
+    totalValue={maxValue}
+    lineWidth={DOUGHNUT_CHART_LINE_WIDTH}
+    label={({ dataEntry }) => percent ? `${dataEntry.value}%` : dataEntry.value}
+    labelStyle={{
+      fontSize,
+      fill: fontColor,
+    }}
+    labelPosition={DOUGHNUT_CHART_LABEL_POSITION}
+    background={background}
+    startAngle={DOUGHNUT_CHART_START_ANGLE}
+    onClick={ props.onClick }
+    />     
+  );
 };
 
 DoughnutChart.defaultProps = {
