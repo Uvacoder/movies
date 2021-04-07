081b2239bcb4e9e35ca3df8bf8f3022061d15252
patrykbura <patrykbura@gmail.com>
2021-04-07 21:45:42 +0200

    change images and fonts RandomMovie component css WIP

--- src/Components/DoughnutChart/DoughnutChart.js
+++ src/Components/DoughnutChart/DoughnutChart.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { PieChart } from 'react-minimal-pie-chart';
 import { getColor } from './DoughnutChartUtil'
+// import { useAnimateProps } from "react-animate-props";
 
 const DOUGHNUT_CHART_LINE_WIDTH = 20;
 const DOUGHNUT_CHART_LABEL_POSITION = 0;
 const DOUGHNUT_CHART_START_ANGLE = 270;
+
 
 const DoughnutChart = (props) => {
   const {
@@ -16,20 +18,30 @@ const DoughnutChart = (props) => {
     background,
     ignoreZeroValue
   } = props;
+  const inputEl = useRef(null);
 
-  if (data === 0) {
-    console.log('ALARM')
-  }
+  // useEffect(() => {
+  //   window.A = inputEl
+  // }, []);
+  
+  // const animatedValue1 = useAnimateProps(Math.min(data, maxValue), {
+  //   ease: Easing.Quad.In,
+  //   delay: 500,
+  //   duration: 300
+  // });
+
+  // if (data === 0) {
+  //   console.log('ALARM')
+  // }
 
 
   // if (ignoreZeroValue && data === 0) {
   //   return null;
   // }
 
-
-
   return (
     <PieChart
+      ref={inputEl}
       data={[{ 
         value: Math.min(data, maxValue), 
         color: getColor(data, maxValue)
